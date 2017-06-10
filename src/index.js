4d86643a0dd1afe4fae1934573e469c55e84faa0
M4steM4 <m4stem4@naver.com>
2017-06-10 23:39:32 +0900

    Create bush

--- src/index.js
+++ src/index.js
@@ -14,3 +14,26 @@ $(window).on('mousewheel', function(e){
         $(this).scrollLeft(-wheelDelta/15 + $(this).scrollLeft());
     }
 });
+
+function addBush() {
+    var bush;
+    for(var i = 0; i < 30; i++) {
+        bush += `<div class="bush"><div class="base"><span></span></div></div>`;
+    }
+    $('#bush').html(bush);
+}
+
+function addTree(location, num) {
+    var tree = "";
+    for(var i = 0; i < num; i++) {
+        tree += `<div class="triangular-tree"> <div class="tree"> <div class="part1 part"><span class="top triangle"></span><span class="bottom triangle"></span></div> <div class="part2 part"><span class="top triangle"></span><span class="bottom triangle"></span><span class="bottom2 triangle"></span></div> <div class="part3 part"><span class="bottom triangle"></span><span class="bottom2 triangle"></span><span class="bottom3 triangle"></span><span class="bottom4 triangle"></span><span class="bottom5 triangle"></span></div> </div> <div class="base"></div> </div>`;
+    }
+    location.html(tree);
+}
+var $line1 = $('.line1');
+var $line2 = $('.line2');
+
+
+addTree($line1, 30);
+addTree($line2, 20);
+addBush();
