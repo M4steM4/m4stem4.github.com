b8b16c88077407b6fcb62629f40fc2b96d05afeb
M4steM4 <m4stem4@naver.com>
2017-06-13 12:20:56 +0900

    t

--- src/index.js
+++ src/index.js
@@ -15,28 +15,45 @@ $(window).on('mousewheel', function(e){
     }
 });
 
-
-function addMiniTree(num) {
-    var miniTree = "";
-    for(var i = 0; i < 30; i++) {
-        miniTree += `<div class="mini-tree"> <div class="tree"></div> <div class="base"></div> </div>`;
-    }
-    $(`.tree${num}`).html(miniTree);
-}
-
 function addTree(location, num) {
     var tree = "";
     for(var i = 0; i < num; i++) {
         tree += `<div class="triangular-tree"> <div class="tree"> <div class="part1 part"><span class="top triangle"></span><span class="bottom triangle"></span></div> <div class="part2 part"><span class="top triangle"></span><span class="bottom triangle"></span><span class="bottom2 triangle"></span></div> <div class="part3 part"><span class="bottom triangle"></span><span class="bottom2 triangle"></span><span class="bottom3 triangle"></span><span class="bottom4 triangle"></span><span class="bottom5 triangle"></span></div> </div> <div class="base"></div> </div>`;
     }
-    tree += `<div class="tree${num}"></div>`;
     location.html(tree);
 }
-var $line1 = $('.line1');
-var $line2 = $('.line2');
+
+function addFox(location, num) {
+    var fox = `<div class="wolf">`;
+    for(var i = 0; i < 3; i++) {
+        fox += `<div class="fox"> <div class="body"> <div class="part part1"></div> <div class="part part2"></div> <div class="part part3"></div> </div> <div class="tail"></div> <div class="legs"></div> </div>`;
+    }
+    fox += `</div>`;
+    var temp = $(`.line${num}`).html();
+    $('.line' + num).html(temp + fox);
+}
+
+function addMiniTree(location, num) {
+    var miniTree = `<div class="mushroom">`;
+    for(var i = 0; i < 21; i++) {
+        miniTree += `<div class="mini-tree"> <div class="tree"></div> <div class="base"></div> </div>`;
+    }
+    miniTree += `</div>`
+    var temp = $(`.line${num}`).html();
+    $('.line' + num).html(temp + miniTree);
+}
 
 
-addTree($line1, 32);
-addTree($line2, 20);
-addMiniTree(32);
-addMiniTree(20);
+
+var minitreeLine;
+var wolfLine;
+var $treeLine1 = $('.line1');
+var $treeLine2 = $('.line2');
+
+
+addTree($treeLine1, 32);
+addTree($treeLine2, 22);
+addFox(wolfLine, 1);
+addFox(wolfLine, 2);
+addMiniTree(minitreeLine, 1);
+addMiniTree(minitreeLine, 2);
